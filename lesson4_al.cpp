76f024d846056dbbea8e6e8fbdc792b0822ddc2b
FerKosNik <77001472+FerKosNik@users.noreply.github.com>
2021-05-24 22:55:39 +0300

    create ls4_al ls5_al

---
+++ lesson4_al.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int routes(int x, int y) {
+    if (x == 0 && y == 0) return 0;
+    else if (x == 0 || y == 0) return 1;
+    else return routes(x, y - 1) + routes(x - 1, y);
+};
+
+int main()
+{
+    const int x = 5;
+    const int y = 5;
+    for (int i = 0; i < x; ++i) {
+        for (int j = 0; j < y; ++j) {
+            cout << routes(i, j);
+            cout << "  ";
+        }
+        cout << endl;
+    };
+
+    return 0;
+}
---
+++ lesson5_al.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+void showArr2(int** arr, size_t sizeX, size_t sizeY) {
+	for (size_t i = 0; i < sizeX; i++)	{
+		for (size_t j = 0; j < sizeY; j++)	{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+void bobleSort(int* arr, int len)
+{
+	for (int i = 0; i < len - 1; ++i){
+		for (size_t i = 0; i < len; i++)
+		{
+
+		}
+		if (arr[i] > arr[i + 1])
+		{
+			swap(arr[i], arr[i + 1]);
+		}
+	}
+	if (len > 0) bobleSort(arr, len - 1);
+}
+
+int main()
+{
+    int arr[3][3] = { 1, 9, 2, 
+						5, 7, 6, 
+							4, 3, 8 };
+	for (size_t i = 0; i < 3; i++) {
+		for (size_t j = 0; j < 3; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+};
+
